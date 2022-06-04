896ac93c5dead2894f94a4d45c6bad6ab0ad58d1
Keisuke.Iwasada <dirty.cross.37@gmail.com>
2022-06-04 18:49:34 +0900

    管理画面

--- webpack.mix.js
+++ webpack.mix.js
@@ -55,3 +55,9 @@ mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'pub
     require('autoprefixer'),
 ]);
 
+
+mix.webpackConfig({
+    stats: {
+         children: true
+    }
+});
