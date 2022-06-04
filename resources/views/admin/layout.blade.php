32bbb4e411cc0cf546b75628c7b16200da4521b5
Keisuke.Iwasada <dirty.cross.37@gmail.com>
2022-06-04 18:04:45 +0900

    管理画面

---
+++ resources/views/admin/layout.blade.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="stylesheet" href="{{ mix('css/common.css') }}">
+        @yield('css')
+        @yield('js')
+        @FilemanagerScript
+    </head>
+    <body>
+        <header>
+            bloGig  管理画面
+        </header>
+
+        <main>
+            @include('admin.partical.sidenavi')
+            <div id="content">
+                @yield('content')
+            </div>
+        </main>
+
+        <footer>
+            bloGig  管理画面
+        </footer>
+    </body>
+</html> 
